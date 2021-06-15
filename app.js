33e0e4d8bebe13d8fb53653be81c9e48b05d334b
caturprasatya <caturprasatya1224@gmail.com>
2021-06-15 17:56:04 +0700

    Create boilerplate code

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+if (["development", "test"].includes(process.env.NODE_ENV)) {
+  require("dotenv").config();
+}
+
 const express = require('express')
+const cors = require("cors");
+
 const route = require('./routes/')
+const errHandler = require('./middlewares/ErrorHanddler')
 
 const app = express()
-const port = process.env.PORT || 3001
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(route)
+app.use(errHandler);
 
-app.listen(port, () => console.log('app run in port: ', port))
+module.exports= app
