7ccf5188c2396a7725016b80dfcde4f29725daa4
caturprasatya <caturprasatya1224@gmail.com>
2021-09-02 07:57:40 +0700

    updated module for deployment

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ let env = process.env.NODE_ENV;
 
 if (["development", "test"].includes(env)) {
   require("dotenv").config();
+  env = env.toUpperCase();
 };
 
-env = env.toUpperCase();
 const dbOption = {
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || null,
-  database: process.env[`DB_DATABSE_${env}`] || "cms_development",
+  database: process.env[`DB_DATABSE_${env}`] || "flex",
   host: process.env.DB_HOST || "127.0.0.1",
   dialect: process.env.DB_DIALECT || "postgres",
 };
